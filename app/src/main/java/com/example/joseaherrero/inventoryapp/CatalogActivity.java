de171b46b95dff0b12e9f0e32bb57811628aca39
Jose A. Herrero <j.herrero.morian@gmail.com>
2017-01-27 13:40:08 +0100

    Code cleanup

--- app/src/main/java/com/example/joseaherrero/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/joseaherrero/inventoryapp/CatalogActivity.java
@@ -6,24 +6,20 @@ import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.support.design.widget.FloatingActionButton;
 
 import com.example.joseaherrero.inventoryapp.data.InventoryContract;
-import com.example.joseaherrero.inventoryapp.data.InventoryDbHelper;
-import com.example.joseaherrero.inventoryapp.data.InventoryProvider;
 
 public class CatalogActivity extends AppCompatActivity  implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final int LOAD_PRODUCTS = 1;
-    private InventoryDbHelper mDbHelper;
     private InventoryCursorAdapter mInventoryCursorAdapter;
 
     @Override
@@ -40,51 +36,44 @@ public class CatalogActivity extends AppCompatActivity  implements LoaderManager
             }
         });
 
-        mDbHelper = new InventoryDbHelper(this);
-
         // Find the ListView which will be populated with the pet data
         ListView inventoryListView = (ListView) findViewById(R.id.list);
         inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                // Create new intent to go to {@link DetailActivity}
-                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
-
                 // Form the content URI that represents the specific pet that was clicked on,
                 // by appending the "id" (passed as input to this method) onto the
                 // {@link InventoryEntry#CONTENT_URI}.
                 Uri currentItemUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, id);
-
-                // Set the URI on the data field of the intent
-                intent.setData(currentItemUri);
-
-                // Launch the {@link DetailActivity} to display the data for the current pet.
-                startActivity(intent);
+                startDetailActivity(currentItemUri);
             }
         });
 
         // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
         View emptyView = findViewById(R.id.empty_view);
         inventoryListView.setEmptyView(emptyView);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
-
-    private void displayDatabaseInfo() {
-        ListView listView = (ListView) findViewById(R.id.list);
         mInventoryCursorAdapter = new InventoryCursorAdapter(this);
-        if(listView.getHeaderViewsCount() == 0) {
-            ViewGroup header = (ViewGroup)getLayoutInflater().inflate(R.layout.list_header, listView, false);
-            listView.addHeaderView(header, null, false);
+
+        // Add header if needed
+        if(inventoryListView.getHeaderViewsCount() == 0) {
+            ViewGroup header = (ViewGroup)getLayoutInflater().inflate(R.layout.list_header, inventoryListView, false);
+            inventoryListView.addHeaderView(header, null, false);
         }
-        listView.setAdapter(mInventoryCursorAdapter);
+        inventoryListView.setAdapter(mInventoryCursorAdapter);
 
         getLoaderManager().initLoader(LOAD_PRODUCTS, null, this);
+    }
 
+    private void startDetailActivity(Uri currentItemUri) {
+        // Create new intent to go to {@link DetailActivity}
+        Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+
+        // Set the URI on the data field of the intent
+        intent.setData(currentItemUri);
+
+        // Launch the {@link DetailActivity} to display the data for the current pet.
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/com/example/joseaherrero/inventoryapp/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/joseaherrero/inventoryapp/InventoryCursorAdapter.java
@@ -1,18 +1,14 @@
 package com.example.joseaherrero.inventoryapp;
 
-import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.CursorLoader;
-import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
