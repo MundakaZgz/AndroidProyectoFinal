622567c65c06e83c29ffeffbfe69fa7fa5c9a363
Jose A. Herrero <joseantonio.herrero@valantmed.com>
2017-02-03 13:58:56 +0100

    Added default locale

--- app/src/main/java/com/example/joseaherrero/inventoryapp/Utils.java
+++ app/src/main/java/com/example/joseaherrero/inventoryapp/Utils.java
@@ -1,5 +1,7 @@
 package com.example.joseaherrero.inventoryapp;
 
+import java.util.Locale;
+
 /**
  * Created by Jose A. Herrero on 20/01/2017.
  */
@@ -7,6 +9,6 @@ package com.example.joseaherrero.inventoryapp;
 public class Utils {
     public static String formatQuantity(int quantity) { return Integer.toString(quantity); }
     public static String formatPrice(float price) {
-        return String.format("$ %.2f", price);
+        return String.format(Locale.getDefault(), "$ %.2f", price);
     }
 }
